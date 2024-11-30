89a865d6071c3ac94a492cef47cc0109501c0628
Joan <79165234+Joangb85@users.noreply.github.com>
2024-11-30 15:55:42 +0000

    Add Navbar component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 import HomePageContent from "../components/HomePageContent"
 import HomePageHeader from "../components/HomePageHeader"
+import { HomePageNavbar } from "../components/HomePageNavbar"
 
 const HomePage = () => {
     return (
         <>
+        <HomePageNavbar />
         <HomePageHeader />
         <HomePageContent/>
         </>
