95fafa858872d930212d159264eb1c7037f4718a
Joan <79165234+Joangb85@users.noreply.github.com>
2024-11-30 18:32:36 +0000

    Adding keys to the cards

--- src/components/HomePageContent.jsx
+++ src/components/HomePageContent.jsx
@@ -1,10 +1,6 @@
 import { Col, Container, Row } from "react-bootstrap";
 import { HomePageCard } from "./HomePageCard"
 
-
-
-
-
 export const HomePageContent = () => {
     const cards = [0, 1, 2, 3];
     return (
@@ -12,7 +8,7 @@ export const HomePageContent = () => {
             <Row>
             {
                 cards.map(
-                    () => <Col><HomePageCard /></Col>
+                    (i) => <Col key={i}><HomePageCard /></Col>
                 )
             }
             </Row>
