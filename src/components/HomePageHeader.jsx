badb3537bb7f13f3df4e33284e3bf0aabcfb6998
Joan <79165234+Joangb85@users.noreply.github.com>
2024-11-30 15:58:14 +0000

    Update Header component

--- src/components/HomePageHeader.jsx
+++ src/components/HomePageHeader.jsx
@@ -1,10 +1,13 @@
+import { Button } from "react-bootstrap"
+
+
 const HomePageHeader = () => {
     return (
         <header>
-        <div>
-            <h1> Hello </h1>
-        </div>
-    </header>
+            <h1> A Warm Welcome! </h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim debitis numquam ipsum odit, beatae eaque voluptatem unde quo repellat nostrum illo sint, eveniet architecto fuga ratione cupiditate dolore recusandae neque.</p>
+            <Button variant="primary">Call to action!</Button>
+        </header>
     )
 }
 
